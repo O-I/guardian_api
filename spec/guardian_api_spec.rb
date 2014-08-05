35b43345b9cb40f4367394fd7b8554a04b8723f9
Rahul Horé <hore.rahul@gmail.com>
2014-08-04 20:14:21 -0400

    Adds tests for .new and .configure

--- spec/guardian_api_spec.rb
+++ spec/guardian_api_spec.rb
@@ -1,7 +1,26 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "GuardianApi" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe Guardian do
+  describe '.new' do
+    it 'returns a Guardian::Client' do
+      expect(Guardian.new).to be_a Guardian::Client
+    end
+  end
+
+  describe '.configure' do
+    it 'sets the api_key' do
+      Guardian.configure do |config|
+        config.api_key = '1234'
+      end
+      expect(Guardian.api_key).to eq '1234'
+    end
+
+    it 'allows setting the base API URL' do
+      expect(Guardian.base_url).to eq 'http://beta.content.guardianapis.com/'
+      Guardian.configure do |config|
+        config.base_url = 'http://content.guardianapis.com/'
+      end
+      expect(Guardian.base_url).to eq 'http://content.guardianapis.com/'
+    end
   end
 end
