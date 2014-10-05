2e65f0c0171c7257ce304ebde23fecbb45858b40
Rahul Horé <hore.rahul@gmail.com>
2014-10-04 20:55:32 -0400

    Updates spec_helper.rb
    Adds `guardian_test_client` and `stub_get` helper methods.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,9 @@ SimpleCov.start
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require 'rspec'
+require 'webmock/rspec'
 require 'guardian_api'
 
 # Requires supporting files with custom matchers and macros, etc,
@@ -19,4 +21,33 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
   config.expect_with(:rspec) { |c| c.syntax = :expect }
+end
+
+WebMock.disable_net_connect!(allow: 'coveralls.io')
+
+def guardian_test_client
+  Guardian::Client.new.tap do |client|
+    client.configure do |config|
+      config.api_key = '1234'
+    end
+  end
+end
+
+AUTH = '?api-key=1234'
+
+def stub_get(path, options = {})
+  file = options.delete(:returns)
+  endpoint = Guardian::Configuration::DEFAULT_BASE_URL + path + AUTH
+  headers = Guardian::Connection::HEADERS
+  stub_request(:get, endpoint)
+    .with(headers: headers)
+    .to_return(body: fixture(file))
+end
+
+def fixture_path
+  File.expand_path('../fixtures', __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
 end
